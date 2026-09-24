73aaa464cc893a68158d18c650158b4dbe0b7da0
Jinusha JJ <ushajinusha@gmail.com>
2026-09-24 19:09:12 +0530

    Implement active navigation on scroll

---
+++ script.js
@@ -0,0 +1,34 @@
+// ===============================
+// ACTIVE NAVIGATION
+// ===============================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar nav a");
+
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
